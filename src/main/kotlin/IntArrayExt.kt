30c87d25c351175022e29d72e90f0826f67fec7f
Chandra Nayak <chandra.nayak@lowes.com>
2019-06-13 10:34:24 +0530

    Added range set extension to IntArray

--- src/main/kotlin/IntArrayExt.kt
+++ src/main/kotlin/IntArrayExt.kt
@@ -1,1 +1,11 @@
+/**
+ * Returns an array containing elements at indices in the specified [indices] range.
+ */
 operator fun IntArray.get(range: IntRange) = sliceArray(range)
+
+/**
+ * Sets the specified value at indices in the specified [indices] range.
+ */
+operator fun IntArray.set(range: IntRange, value: Int) {
+    range.forEach { this[it] = value }
+}
--- src/test/kotlin/IntArrayExtTest.kt
+++ src/test/kotlin/IntArrayExtTest.kt
@@ -35,4 +35,39 @@ class IntArrayExtTest {
             arr[3..20]
         }
     }
+
+    @Test
+    fun `invalid range access should return empty array`() {
+        val arr = IntArray(10) { it }
+        assertArrayEquals(intArrayOf(), arr[3..1])
+    }
+
+    @Test
+    fun `range set inclusively`() {
+        val arr = IntArray(10) { it }
+        arr[3..7] = 20
+        assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 20, 8, 9), arr)
+    }
+
+    @Test
+    fun `range set exclusively`() {
+        val arr = IntArray(10) { it }
+        arr[3 until 7] = 20
+        assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 7, 8, 9), arr)
+    }
+
+    @Test
+    fun `out of range set throws exception`() {
+        val arr = IntArray(10) { it * it }
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            arr[3 until 20] = 30
+        }
+    }
+
+    @Test
+    fun `invalid range set should not do anything`() {
+        val arr = IntArray(10) { it * it }
+        arr[7 until 3] = 34
+        assertArrayEquals(IntArray(10) { it * it }, arr)
+    }
 }
