c38e0bc7685b910442aff92eb9ddc539e6842a33
Chandra Nayak <chandra.nayak@lowes.com>
2019-07-02 16:21:17 +0530

    Some minor modification on import statement

--- src/test/kotlin/kmat/array/IntArrayUpdateTest.kt
+++ src/test/kotlin/kmat/array/IntArrayUpdateTest.kt
@@ -1,8 +1,8 @@
 package kmat.array
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import java.lang.IndexOutOfBoundsException
 
 class IntArrayUpdateTest {
 
@@ -10,20 +10,20 @@ class IntArrayUpdateTest {
     fun `range set inclusively`() {
         val arr = IntArray(10) { it }
         arr[3..7] = 20
-        Assertions.assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 20, 8, 9), arr)
+        assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 20, 8, 9), arr)
     }
 
     @Test
     fun `range set exclusively`() {
         val arr = IntArray(10) { it }
         arr[3 until 7] = 20
-        Assertions.assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 7, 8, 9), arr)
+        assertArrayEquals(intArrayOf(0, 1, 2, 20, 20, 20, 20, 7, 8, 9), arr)
     }
 
     @Test
     fun `out of range set throws exception`() {
         val arr = IntArray(10) { it * it }
-        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+        assertThrows(IndexOutOfBoundsException::class.java) {
             arr[3 until 20] = 30
         }
     }
@@ -32,6 +32,6 @@ class IntArrayUpdateTest {
     fun `invalid range set should not do anything`() {
         val arr = IntArray(10) { it * it }
         arr[7 until 3] = 34
-        Assertions.assertArrayEquals(IntArray(10) { it * it }, arr)
+        assertArrayEquals(IntArray(10) { it * it }, arr)
     }
 }
